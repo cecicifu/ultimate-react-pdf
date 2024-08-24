85cb46e4256ed4fd2c6d9a4de4bc99910f2d1f2a
Ceci Cifu <ceciliocifuentes@gmail.com>
2024-08-25 00:21:03 +0200

    refactor: add StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,17 @@
 import "./index.css"
 
+import { StrictMode } from "react"
 import ReactDOM from "react-dom/client"
 
-import App from "./App"
+import { Document } from "@/Document"
+import { Page } from "@/Page"
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />)
+import { examplePdf } from "./example"
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<Document src={{ data: atob(examplePdf) }}>
+			<Page controls />
+		</Document>
+	</StrictMode>
+)
